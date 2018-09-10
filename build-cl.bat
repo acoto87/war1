737bba6c3b73e46c8b10d81ee7f36abf7d747ce9
acoto87 <acoto87@gmail.com>
2018-09-10 01:07:45 -0500

    - Commit of the build-*.bat files

--- build-cl.bat
+++ build-cl.bat
@@ -5,17 +5,19 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Bu
 popd
 SET path=D:\Work\warcraft;%path%
 
+REM Include paths
 SET GLEWIncludePath=D:\Work\libs\glew32\include
-SET GLEWLibPath=D:\Work\libs\glew32\lib
-
 SET GLFWIncludePath=D:\Work\libs\glfw-3.2.1_x64\include
+SET GLMIncludePath=D:\Work\cglm\include\cglm
+SET STBIncludePath=D:\Work\stb
+SET SHLIncludePath=D:\Work\shl
+SET NVGIncludePath=D:\Work\nanovg\src
+
+REM Lib paths
+SET GLEWLibPath=D:\Work\libs\glew32\lib
 SET GLFWLibPath=D:\Work\libs\glfw-3.2.1_x64\lib-vc2015
 
-SET GLMIncludePath=D:\Work\cglm\include\cglm
-
-SET STBIncludePath=D:\Work\stb
-
-SET CommonCompilerFlags=-Od -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -FC -Z7 -TC -I %GLEWIncludePath% -I %GLFWIncludePath% -I %GLMIncludePath% -I %STBIncludePath% -D _DEBUG
+SET CommonCompilerFlags=-Od -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -FC -Z7 -TC -I %GLEWIncludePath% -I %GLFWIncludePath% -I %GLMIncludePath% -I %STBIncludePath% -I %SHLIncludePath% -I %NVGIncludePath% -D _DEBUG -W2
 SET CommonLinkerFlags= -incremental:no -opt:ref -out:war1.exe -pdb:war1.pdb -libpath:%GLEWLibPath% -libpath:%GLFWLibPath% user32.lib gdi32.lib opengl32.lib glew32.lib glfw3dll.lib
 
 IF NOT EXIST build mkdir build
@@ -31,6 +33,9 @@ REM 	-O1 maximum optimizations (favor space)
 REM 	-O2 maximum optimizations (favor speed)
 REM 	-Od disable optimizations (default)
 REM 	-Oi[-] enable intrinsic functions
+
+REM Enable all warnings
+REM     -Wall
 
 REM Disable all warnings
 REM 	-w
--- build-gcc.bat
+++ build-gcc.bat
@@ -1,9 +1,22 @@
 @echo off
 
-SET CommonCompilerFlags=-O -Wall -std=c99
+SET path=D:\Work\warcraft;%path%
+
+SET GLEWIncludePath=D:\Work\libs\glew32\include
+SET GLEWLibPath=D:\Work\libs\glew32\lib
+
+SET GLFWIncludePath=D:\Work\libs\glfw-3.2.1_x64\include
+SET GLFWLibPath=D:\Work\libs\glfw-3.2.1_x64\lib-mingw-w64
+
+SET GLMIncludePath=D:\Work\cglm\include\cglm
+
+SET STBIncludePath=D:\Work\stb
+
+SET CommonCompilerFlags=-O -Wall -x c -I %GLEWIncludePath% -I %GLFWIncludePath% -I %GLMIncludePath% -I %STBIncludePath%
+SET CommonLinkerFlags= -L:%GLEWLibPath% -L:%GLFWLibPath% -l user32.lib -l gdi32 -l opengl32 -l glew32.lib -l libglfw3dll
 
 IF NOT EXIST build mkdir build
 pushd build
 
-gcc %CommonCompilerFlags% ../src/war1.c -o war1.exe
+gcc %CommonCompilerFlags% %CommonLinkerFlags% ../src/war1.c -o war1.exe
 popd
